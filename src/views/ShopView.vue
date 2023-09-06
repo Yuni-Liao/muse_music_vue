6f5a563cc32f311c4c7b91aa9e9deebd6a1378ea
kay <candy168741@gmail.com>
2023-09-06 13:04:20 +0800

    add ShopView.vue 周邊販售頁面切版 add assets/image/ShopImage 周邊販售圖片資料夾

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,101 +1,64 @@
 <template>
     <!-- <h1>這是商城頁面</h1> -->
-            <!-- 注意:: 點擊商品>查看商品詳情的內頁跳轉要用router-link to="___" 這方式套才不會404, 用<a>沒辦法 -->
-        <!-- <router-link to="/shopProd/1"><img alt="Vue logo" src="~@/assets/image/muse_logo.png"></router-link> -->
+    <!-- 注意:: 點擊商品>查看商品詳情的內頁跳轉要用router-link to="___" 這方式套才不會404, 用<a>沒辦法 -->
+    <!-- <router-link to="/shopProd/1"><img alt="Vue logo" src="~@/assets/image/muse_logo.png"></router-link> -->
+    <div class="bannerpic">
+        <img src="~@/assets/image/ShopImage/shopbanner.png" alt="周邊販售">
+    </div>
+    <Breadcrumb separator="/">
+        <BreadcrumbItem><router-link to="/">首頁</router-link></BreadcrumbItem>
+        <BreadcrumbItem>周邊商品</BreadcrumbItem>
+    </Breadcrumb>
+    <div class="selectArea">
+        <select class="prodSort obj_Radius" v-model="selectedType">
+            <option v-for="(type, index) in typeOptions" :value="type" :key="index">{{ type }}</option>
+        </select>
+
+        <select class="prodQuantity obj_Radius" v-model="selectedPerPage">
+            <option v-for="(quantity, option) in perPageOptions" :value="option">{{ option }}</option>
+        </select>
+    </div>
+
+
     <div class="shop">
-        <div class="prodtype ">
+        <div class="prodType ">
             <h2>商品類型</h2>
-            <a href="#">新貨上架</a>
-            <a href="#">黑膠唱片</a>
-            <a href="#">男藝人</a>
-            <a href="#">女藝人</a>
-            <a href="#">樂團團體</a>
+            <ul>
+                <li><a href="#">新貨上架</a></li>
+                <li><a href="#">黑膠唱片</a></li>
+                <li><a href="#">男藝人</a></li>
+                <li><a href="#">女藝人</a></li>
+                <li><a href="#">樂團團體</a></li>
+            </ul>
         </div>
         <div class="inner">
-        <div class="top">
-            <Breadcrumb separator="/">
-                <BreadcrumbItem><router-link to="/">首頁</router-link></BreadcrumbItem>
-                <BreadcrumbItem>周邊商品</BreadcrumbItem>
-            </Breadcrumb>
-            <select class="obj_Radius">
-                <option selected>商品排序</option>
-                <option>上架時間(新>舊)</option>
-                <option>上架時間(舊>新)</option>
-                <option>價格:由高到低</option>
-                <option>價格:由低到高</option>
-            </select>
-            <select class="obj_Radius">
-                <option selected>每頁顯示20個</option>
-                <option>每頁顯示16個</option>
-                <option>每頁顯示12個</option>
-                <option>每頁顯示8個</option>
-            </select>
-        </div>
-        
-        <div class="prod">
-            <div class="prodcard">
-                <div class="prodpic">
-                    <img alt="Vue logo" src="https://picsum.photos/247/163">
-                </div>
-                <div class="prodtxt">
-                    <p>商品名稱</p>
-                    <p>$990</p>
-                    <span class="prodcount">
-                        <button class="prodbtn"><img src="~@/assets/image/ShopImage/down.png"></button>
-                        <p>0</p>
-                        <button class="prodbtn"><img src="~@/assets/image/ShopImage/up.png"></button>
-                    </span>
+            <div class="prod">
+                <div class="prodCard" v-for="(item, index) in products" :key="index">
+                    <div class="prodPic">
+                        <img :src="require(`@/assets/image/ShopImage/${item.prodPic}`)" alt="item.prodName">
+                    </div>
+                    <div class="prodTxt">
+                        <p class="prodName">{{ item.prodName }}</p>
+                        <p class="prodPrice">$ {{ item.prodPrice }}</p>
+                        <span class="prodCount">
+                            <button class="prodBtn" @click="decrementItem(item)"><img
+                                    src="~@/assets/image/ShopImage/down.png"></button>
+                            <p>{{ item.inCart }}</p>
+                            <button class="prodBtn" @click="incrementItem(item)"><img
+                                    src="~@/assets/image/ShopImage/up.png"></button>
+                        </span>
+                        <button class="obj_Radius btn_Shop_Border">加入購物車</button>
+                    </div>
                 </div>
             </div>
-            <div class="prodcard">
-                <div class="prodpic">
-                    <img alt="Vue logo" src="https://picsum.photos/247/163">
-                </div>
-                <div class="prodtxt">
-                    <p>商品名稱</p>
-                    <p>$990</p>
-                    <span class="prodcount">
-                        <button class="prodbtn"><img src="~@/assets/image/ShopImage/down.png"></button>
-                        <p>0</p>
-                        <button class="prodbtn"><img src="~@/assets/image/ShopImage/up.png"></button>
-                    </span>
-                </div>
-            </div>
-            <div class="prodcard">
-                <div class="prodpic">
-                    <img alt="Vue logo" src="https://picsum.photos/247/163">
-                </div>
-                <div class="prodtxt">
-                    <p>商品名稱</p>
-                    <p>$990</p>
-                    <span class="prodcount">
-                        <button class="prodbtn"><img src="~@/assets/image/ShopImage/down.png"></button>
-                        <p>0</p>
-                        <button class="prodbtn"><img src="~@/assets/image/ShopImage/up.png"></button>
-                    </span>
-                </div>
-            </div>
-            <div class="prodcard">
-                <div class="prodpic">
-                    <img alt="Vue logo" src="https://picsum.photos/247/163">
-                </div>
-                <div class="prodtxt">
-                    <p>商品名稱</p>
-                    <p>$990</p>
-                    <span class="prodcount">
-                        <button class="prodbtn"><img src="~@/assets/image/ShopImage/down.png"></button>
-                        <p>0</p>
-                        <button class="prodbtn"><img src="~@/assets/image/ShopImage/up.png"></button>
-                    </span>
-                </div>
-            </div>
+            <!-- 以下是分頁面區 -->
+            <Page :total="totalItems" :current.sync="currentPage" :page-size="pageSize" />
         </div>
     </div>
-</div>
 </template>
 
 
 <style scoped lang="scss">
 @import "~@/assets/scss/page/shop.scss";
 </style>
-<script src="./js/ShopView"></script>
+<script src="./js/ShopView.js"></script>
--- src/views/js/ShopView.js
+++ src/views/js/ShopView.js
@@ -1,3 +1,164 @@
 export default {
-    //  script寫這
-}
+    data() {
+        return {
+            products: [
+                {
+                    id: 1,
+                    prodPic: "prod1.jpg",
+                    prodName: "Killbeast 黑膠",
+                    prodPrice: 990,
+                    inCart: 0,
+                },
+                {
+                    id: 2,
+                    prodPic: "prod2.jpg",
+                    prodName: "西城男人 黑膠",
+                    prodPrice: 990,
+                    inCart: 0,
+                },
+                {
+                    id: 3,
+                    prodPic: "prod3.jpg",
+                    prodName: "Party Time 黑膠",
+                    prodPrice: 990,
+                    inCart: 0,
+                },
+                {
+                    id: 4,
+                    prodPic: "prod4.jpg",
+                    prodName: "Rose 黑膠",
+                    prodPrice: 990,
+                    inCart: 0,
+                },
+                {
+                    id: 5,
+                    prodPic: "prod5.jpg",
+                    prodName: "Over Over 黑膠",
+                    prodPrice: 990,
+                    inCart: 0,
+                },
+                {
+                    id: 6,
+                    prodPic: "prod6.jpg",
+                    prodName: "Blackmage 黑膠",
+                    prodPrice: 990,
+                    inCart: 0,
+                },
+                {
+                    id: 7,
+                    prodPic: "prod7.jpg",
+                    prodName: "KKOCK 黑膠",
+                    prodPrice: 990,
+                    inCart: 0,
+                },
+                {
+                    id: 8,
+                    prodPic: "prod1.jpg",
+                    prodName: "cillbeast 黑膠",
+                    prodPrice: 990,
+                    inCart: 0,
+                },
+                {
+                    id: 9,
+                    prodPic: "prod2.jpg",
+                    prodName: "東城男子 黑膠",
+                    prodPrice: 990,
+                    inCart: 0,
+                },
+                {
+                    id: 10,
+                    prodPic: "prod3.jpg",
+                    prodName: "Sleep Time 黑膠",
+                    prodPrice: 990,
+                    inCart: 0,
+                },
+                {
+                    id: 11,
+                    prodPic: "prod4.jpg",
+                    prodName: "Lisa 黑膠",
+                    prodPrice: 990,
+                    inCart: 0,
+                },
+                {
+                    id: 12,
+                    prodPic: "prod5.jpg",
+                    prodName: "Very Very 黑膠",
+                    prodPrice: 990,
+                    inCart: 0,
+                },
+                {
+                    id: 13,
+                    prodPic: "prod6.jpg",
+                    prodName: "Pinkmage 黑膠",
+                    prodPrice: 990,
+                    inCart: 0,
+                },
+                {
+                    id: 14,
+                    prodPic: "prod7.jpg",
+                    prodName: "VVOCK 黑膠",
+                    prodPrice: 990,
+                    inCart: 0,
+                },
+                {
+                    id: 15,
+                    prodPic: "prod1.jpg",
+                    prodName: "Drop beast 黑膠",
+                    prodPrice: 990,
+                    inCart: 0,
+                },
+                {
+                    id: 16,
+                    prodPic: "prod2.jpg",
+                    prodName: "北區女孩 黑膠",
+                    prodPrice: 990,
+                    inCart: 0,
+                },
+                {
+                    id: 17,
+                    prodPic: "prod3.jpg",
+                    prodName: "Show Time 黑膠",
+                    prodPrice: 990,
+                    inCart: 0,
+                },
+                {
+                    id: 18,
+                    prodPic: "prod4.jpg",
+                    prodName: "Jisso 黑膠",
+                    prodPrice: 990,
+                    inCart: 0,
+                },
+                {
+                    id: 19,
+                    prodPic: "prod5.jpg",
+                    prodName: "Money Money 黑膠",
+                    prodPrice: 990,
+                    inCart: 0,
+                },
+                {
+                    id: 20,
+                    prodPic: "prod6.jpg",
+                    prodName: "Bluemage 黑膠",
+                    prodPrice: 990,
+                    inCart: 0,
+                }
+            ],
+            //切換數量功能還在研究中QQ
+            perPageOptions: {
+                '每頁顯示20個': 20,
+                '每頁顯示16個': 16,
+                '每頁顯示12個': 12,
+                '每頁顯示8個': 8,
+            },
+            selectedPerPage: '每頁顯示20個',// 初始选择
+            typeOptions: [
+                '商品排序',
+                '上架時間(新>舊)',
+                '上架時間(舊>新)',
+                '價格:由高到低',
+                '價格:由低到高',
+            ],
+              selectedType: '商品排序', // 初始选择
+        }
+    }
+};
