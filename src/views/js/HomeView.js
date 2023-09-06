14a9b77e5579107731cef008ba3a5c1f2f0ad909
rou94980 <rou94980@gmail.com>
2023-09-06 14:27:32 +0800

    add 首頁新歌推薦、本週熱門歌曲輪播

--- src/views/js/HomeView.js
+++ src/views/js/HomeView.js
@@ -1,3 +1,94 @@
+//引入swiper-------------------------
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+// import required modules
+import { EffectCoverflow, Pagination } from "swiper/modules";
+
+// -----------------------------------
+
 export default {
-    //  script寫這
-}
+  data() {
+    return {
+      songs: [
+        {
+          title: "宇宙飛行1",
+          image: "muse_logo.png",
+          link: "https://tw.yahoo.com",
+          singer: "桌子樂團",
+          views: 0,
+        },
+        {
+          title: "宇宙飛行2",
+          image: "muse_logo.png",
+          link: "https://tw.yahoo.com",
+          singer: "桌子樂團",
+          views: 0,
+        },
+        {
+          title: "宇宙飛行3",
+          image: "muse_logo.png",
+          link: "https://tw.yahoo.com",
+          singer: "桌子樂團",
+          views: 0,
+        },
+        {
+          title: "宇宙飛行4",
+          image: "muse_logo.png",
+          link: "https://tw.yahoo.com",
+          singer: "桌子樂團",
+          views: 0,
+        },
+        {
+          title: "宇宙飛行5",
+          image: "muse_logo.png",
+          link: "https://tw.yahoo.com",
+          singer: "桌子樂團",
+          views: 0,
+        },
+        {
+          title: "宇宙飛行6",
+          image: "muse_logo.png",
+          songLink: "https://tw.yahoo.com",
+          singer: "桌子樂團7",
+          views: 0,
+        },
+        {
+          title: "宇宙飛行8",
+          image: "muse_logo.png",
+          songLink: "https://tw.yahoo.com",
+          singer: "桌子樂團",
+          views: 0,
+        },
+        {
+          title: "宇宙飛行9",
+          image: "muse_logo.png",
+          songLink: "https://tw.yahoo.com",
+          singer: "桌子樂團",
+          views: 0,
+        },
+        {
+          title: "宇宙飛行10",
+          image: "muse_logo.png",
+          link: "https://tw.yahoo.com",
+          singer: "桌子樂團",
+          views: 0,
+        },
+      ],
+    };
+  },
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [EffectCoverflow, Pagination],
+    };
+  },
+};
