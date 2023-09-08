8c025aeba34b73a17431f4cc4ca70cb172b808a4
rou94980 <rou94980@gmail.com>
2023-09-08 19:39:17 +0800

    add 我的音樂庫-我的歌單頁面

--- src/router/index.js
+++ src/router/index.js
@@ -1,60 +1,69 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'index',
-    component: HomeView
+    path: "/",
+    name: "index",
+    component: HomeView,
   },
   {
-    path: '/find',
-    name: 'find',
+    path: "/find",
+    name: "find",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "find" */ '../views/FindView.vue')
+    component: () =>
+      import(/* webpackChunkName: "find" */ "../views/FindView.vue"),
   },
   {
-    path: '/ranking',
-    name: 'ranking',
-    component: () => import(/* webpackChunkName: "ranking" */ '../views/RankingView.vue')
+    path: "/ranking",
+    name: "ranking",
+    component: () =>
+      import(/* webpackChunkName: "ranking" */ "../views/RankingView.vue"),
   },
   {
-    path: '/shop',
-    name: 'shop',
-    component: () => import(/* webpackChunkName: "shop" */ '../views/ShopView.vue')
+    path: "/shop",
+    name: "shop",
+    component: () =>
+      import(/* webpackChunkName: "shop" */ "../views/ShopView.vue"),
   },
   {
-    path: '/activity',
-    name: 'activity',
-    component: () => import(/* webpackChunkName: "activity" */ '../views/ActivityView.vue')
+    path: "/activity",
+    name: "activity",
+    component: () =>
+      import(/* webpackChunkName: "activity" */ "../views/ActivityView.vue"),
   },
   {
-    path: '/login',
-    name: 'login',
-    component: () => import(/* webpackChunkName: "login" */ '../views/LoginView.vue')
+    path: "/login",
+    name: "login",
+    component: () =>
+      import(/* webpackChunkName: "login" */ "../views/LoginView.vue"),
   },
   {
-    path: '/register',
-    name: 'register',
-    component: () => import(/* webpackChunkName: "register" */ '../views/RegisterView.vue')
+    path: "/register",
+    name: "register",
+    component: () =>
+      import(/* webpackChunkName: "register" */ "../views/RegisterView.vue"),
   },
   {
-    path: '/forgetpsw',
-    name: 'forgetpsw',
-    component: () => import(/* webpackChunkName: "forgetpsw" */ '../views/ForgetPsw.vue')
+    path: "/forgetpsw",
+    name: "forgetpsw",
+    component: () =>
+      import(/* webpackChunkName: "forgetpsw" */ "../views/ForgetPsw.vue"),
   },
   {
-    path: '/about',
-    name: 'about',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: "/about",
+    name: "about",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
   {
-    //faq的component還沒改還沒導,試試看能不能用茅點 
-    path: '/faq',
-    name: 'faq',
-    component: () => import(/* webpackChunkName: "faq" */ '../views/AboutView.vue')
+    //faq的component還沒改還沒導,試試看能不能用茅點
+    path: "/faq",
+    name: "faq",
+    component: () =>
+      import(/* webpackChunkName: "faq" */ "../views/AboutView.vue"),
   },
   // {
   //   //隱私權政策還沒導,之後再確認看是不是直接寫彈窗就好
@@ -65,33 +74,43 @@ const routes = [
   // },
 
   {
-    path: '/backend',
-    name: 'backend',
-    component: () => import(/* webpackChunkName: "backend" */ '../views/Backend.vue')
+    path: "/backend",
+    name: "backend",
+    component: () =>
+      import(/* webpackChunkName: "backend" */ "../views/Backend.vue"),
   },
 
   // 以下是商城商品詳情頁面,看想做幾個產品請自行創建
   {
-    path: '/shopProd/1',
-    name: 'shop_prod_1',
-    component: () => import(/* webpackChunkName: "shop_prod_1" */ '../views/shopProd/1.vue')
+    path: "/shopProd/1",
+    name: "shop_prod_1",
+    component: () =>
+      import(/* webpackChunkName: "shop_prod_1" */ "../views/shopProd/1.vue"),
+  },
+
+  //以下是我的音樂庫-我的歌單
+  {
+    path: "/mysonglist",
+    name: "mysonglist",
+    component: () =>
+      import(/* webpackChunkName: "mysonglist" */ "../views/MySonglist.vue"),
   },
 
   //若網址列亂輸入,就會跳到404頁面,此區塊請固定放最下方
   {
-    path: '/:pathMatch(.*)*',
-    name: 'NotFound',
-    component: () => import(/* webpackChunkName: "error" */ '../views/NotFoundView.vue')
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: () =>
+      import(/* webpackChunkName: "error" */ "../views/NotFoundView.vue"),
   },
-]
-
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
   scrollBehavior(to, from, savedPosition) {
-    return { top: 0 }
+    return { top: 0 };
   },
-})
+});
 
-export default router
+export default router;
---
+++ src/views/js/MySonglistView.js
@@ -0,0 +1,16 @@
+export default {
+  data() {
+    return {
+      test: "123",
+    };
+  },
+  //   methods: {
+  //       next() {
+  //           if (this.current == 3) {
+  //               this.current = 0;
+  //           } else {
+  //               this.current += 1;
+  //           }
+  //       }
+  //   }
+};
