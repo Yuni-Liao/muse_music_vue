607cd650ea27008c94bffabc592422bd2714fd2f
rou94980 <rou94980@gmail.com>
2023-09-09 11:06:18 +0800

    add 播放鍵(大)組件,我的歌單篩選功能

--- src/views/MySonglist.vue
+++ src/views/MySonglist.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="mysonglist">
     <section>
-      <h1>我的歌單{{ test }}</h1>
-      <div>
-        <button><h2>全部</h2></button>
-        <button><h2>由我建立</h2></button>
-        <button><h2>我追蹤的</h2></button>
-      </div>
+      <h1>我的歌單</h1>
       <div class="tab">
-        <button class="item"></button>
+        <button @click="currentType = 0"><h2>全部</h2></button>
+        <button @click="currentType = 1"><h2>由我建立</h2></button>
+        <button @click="currentType = 2"><h2>我追蹤的</h2></button>
       </div>
     </section>
-    <section class="list">
-      <div class="sl-item"></div>
+    <section class="sl-list">
+      <div class="sl-item" v-for="(item, index) in choosecreator">
+        <div class="pic">
+          <img :src="require(`/public/image/index/${item.image}`)" alt="" />
+          <PlayBtnBig></PlayBtnBig>
+        </div>
+        <div>
+          <h3>{{ item.slname }}</h3>
+          <span>共有{{ item.songnum }}首歌</span>
+        </div>
+        <p>{{ item.creator }}</p>
+      </div>
     </section>
   </div>
 </template>
--- src/views/js/MySonglistView.js
+++ src/views/js/MySonglistView.js
@@ -1,16 +1,67 @@
+import PlayBtnBig from "@/components/PlayBtnBig.vue";
+
 export default {
+  components: { PlayBtnBig },
   data() {
     return {
-      test: "123",
+      currentType: 0, //0,1,2
+      songlists: [
+        {
+          slid: 1,
+          slname: "我的早晨歌單",
+          image: "song01.jpg",
+          memid: 1,
+          creator: "我",
+          playnum: 0,
+          songnum: 123,
+        },
+        {
+          slid: 2,
+          slname: "亨利的早晨歌單",
+          image: "song01.jpg",
+          memid: 2,
+          creator: "亨利",
+          playnum: 0,
+          songnum: 123,
+        },
+        {
+          slid: 3,
+          slname: "大衛的早晨歌單",
+          image: "song01.jpg",
+          memid: 3,
+          creator: "大衛",
+          playnum: 0,
+          songnum: 123,
+        },
+        {
+          slid: 4,
+          slname: "我的睡前歌單",
+          image: "song01.jpg",
+          memid: 1,
+          creator: "我",
+          playnum: 0,
+          songnum: 123,
+        },
+        {
+          slid: 5,
+          slname: "我的假日歌單",
+          image: "song01.jpg",
+          memid: 1,
+          creator: "我",
+          playnum: 0,
+          songnum: 123,
+        },
+      ],
     };
   },
-  //   methods: {
-  //       next() {
-  //           if (this.current == 3) {
-  //               this.current = 0;
-  //           } else {
-  //               this.current += 1;
-  //           }
-  //       }
-  //   }
+  computed: {
+    choosecreator() {
+      // 使用 Array.filter() 過濾 memid 為 1 的資料
+      return this.songlists.filter((item, index, array) => {
+        if (this.currentType === 0) return true;
+        if (this.currentType === 1) return item.memid == 1;
+        return item.memid !== 1;
+      });
+    },
+  },
 };
