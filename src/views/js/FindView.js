cb56d53137b31a29604dbc2135e0d0d459873016
karta1020146 <karta1020146@gmail.com>
2023-09-09 17:03:48 +0800

    update find

--- src/views/js/FindView.js
+++ src/views/js/FindView.js
@@ -1,3 +1,129 @@
 export default {
-    //  script寫這
+    data() {
+        return {
+            // 語系
+            languages:[
+                {
+                    id: 1,
+                    name:'日語流行',
+                    imgUrl:123,
+                    link: '/'
+                },
+                {
+                    id: 2,
+                    name:'韓語流行',
+                    imgUrl:123,
+                    link: '/'
+                },
+                {
+                    id: 3,
+                    name:'華語流行',
+                    imgUrl:123,
+                    link: '/'
+                },
+                {
+                    id: 4,
+                    name:'西洋流行',
+                    imgUrl:123,
+                    link: '/'
+                },
+                {
+                    id: 5,
+                    name: '其他語言',
+                    imgUrl: 123,
+                    link: '/'
+                }
+            ],
+            // 風格
+            styles:[
+                {
+                    id: 1,
+                    name: '流行音樂',
+                    imgUrl: 123,
+                    link: '/'
+                },
+                {
+                    id: 2,
+                    name: '獨立音樂',
+                    imgUrl: 123,
+                    link: '/'
+                },
+                {
+                    id: 3,
+                    name: '嘻哈',
+                    imgUrl: 123,
+                    link: '/'
+                },
+                {
+                    id: 4,
+                    name: '搖滾',
+                    imgUrl: 123,
+                    link: '/'
+                },
+                {
+                    id: 5,
+                    name: '藍調',
+                    imgUrl: 123,
+                    link: '/'
+                },
+                {
+                    id: 6,
+                    name: '爵士',
+                    imgUrl: 123,
+                    link: '/'
+                },
+                {
+                    id: 7,
+                    name: '古典',
+                    imgUrl: 123,
+                    link: '/'
+                },
+            ],
+            // 心情
+            emotion:[
+                {
+                    id: 1,
+                    name: '快樂',
+                    imgUrl: 123,
+                    link: '/'
+                },
+                {
+                    id: 2,
+                    name: '傷心',
+                    imgUrl: 123,
+                    link: '/'
+                },
+                {
+                    id: 3,
+                    name: '運動',
+                    imgUrl: 123,
+                    link: '/'
+                },
+                {
+                    id: 4,
+                    name: '輕鬆',
+                    imgUrl: 123,
+                    link: '/'
+                },
+                {
+                    id: 5,
+                    name: '舒眠',
+                    imgUrl: 123,
+                    link: '/'
+                },
+                {
+                    id: 6,
+                    name: '專注',
+                    imgUrl: 123,
+                    link: '/'
+                },
+                {
+                    id: 7,
+                    name: '派對',
+                    imgUrl: 123,
+                    link: '/'
+                },
+            ]
+        }
+    },
 }
