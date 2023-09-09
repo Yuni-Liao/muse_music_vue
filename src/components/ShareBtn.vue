7d92315fc8580c25f6fb9338c8d6a740935dac7c
rou94980 <rou94980@gmail.com>
2023-09-10 02:30:51 +0800

    add 播放鍵(大).加入歌單.加入我的最愛.分享 組件

---
+++ src/components/ShareBtn.vue
@@ -0,0 +1,22 @@
+<template>
+  <button id="ShareBtn" @click="ShareBtn()">
+    <fontAwesome :icon="['fa', 'share']" style="color: #252525" />
+  </button>
+</template>
+<script>
+export default {
+  name: "ShareBtn",
+  methods: {
+    ShareBtn() {
+      // 此處撰寫功能code，測試用內容可清除
+      alert("分享");
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+#ShareBtn {
+  @include music_btn_circle(35px);
+}
+</style>
