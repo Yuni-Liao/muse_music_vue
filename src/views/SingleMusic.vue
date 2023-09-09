0187a6688fd6936d890ec7c7e4a2313a07796d15
kay <candy168741@gmail.com>
2023-09-09 10:40:11 +0800

    add 新增單曲頁面相關頁面

---
+++ src/views/SingleMusic.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="singleMusic">
+
+    </div>
+</template>
+<style scoped lang="scss">@import "~@/assets/scss/page/_singlemusic.scss";</style>
+<script src="./js/MySonglistView.js"></script>
