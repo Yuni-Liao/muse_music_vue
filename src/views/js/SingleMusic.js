1ddabdcf8bb9b0a5d6e73d928c1e121fcdf57c1e
kay <candy168741@gmail.com>
2023-09-10 16:25:47 +0800

    add 單曲頁面 引入共用按鈕元件

---
+++ src/views/js/SingleMusic.js
@@ -0,0 +1,9 @@
+import PlayBtnBig from "@/components/PlayBtnBig.vue";
+import AddFavBtn from "@/components/AddFavBtn.vue";
+import AddSlBtn from "@/components/AddSlBtn.vue";
+import ShareBtn from "@/components/ShareBtn.vue";
+
+export default {
+    components: { PlayBtnBig, AddFavBtn, AddSlBtn, ShareBtn },
+
+}
