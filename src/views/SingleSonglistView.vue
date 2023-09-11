ac09185844881fe3e3d45ba49393410872547504
rou94980 <rou94980@gmail.com>
2023-09-11 13:38:11 +0800

    add 歌單頁面

---
+++ src/views/SingleSonglistView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="singlesonglist">
+    <h1>這是歌單頁面</h1>
+  </div>
+</template>
+<style scoped lang="scss">
+@import "~@/assets/scss/page/singlesonglist";
+</style>
+<script src="./js/SingleSonglistView"></script>
