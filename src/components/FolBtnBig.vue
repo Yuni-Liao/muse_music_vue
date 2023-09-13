9cccfa25551f3576e149e94fbaceeff7cba86531
rou94980 <rou94980@gmail.com>
2023-09-14 02:27:13 +0800

    add FolBtnBig 追蹤按鈕(大)

--- src/components/FolBtnBig.vue
+++ src/components/FolBtnBig.vue
@@ -5,29 +5,59 @@
     :class="{ active: isFol }"
     @click="toggleFol()"
   >
-    <fontAwesome class="i" :icon="['fa', 'plus']" /> 追蹤
+    <fontAwesome v-show="!isFol" class="i" :icon="['fa', 'plus']" />
+    {{ isFol ? " 已追蹤 " : "追蹤" }}
   </button>
 </template>
 <script>
+//還沒寫完，還要從父取值去控制按鈕功能
+
 export default {
   name: "FolBtnBig",
   data() {
     return {
       isFol: false,
-      functype: 0, //0追蹤歌單, 1追蹤創作者, 2追蹤音樂快訊
+      functype: 2, //0追蹤歌單, 1追蹤創作者, 2追蹤音樂快訊
     };
-  },
-  computed: {
-    // AddSlBtnStyle() {
-    //   return {
-    //     color: this.toggleFol ? "red" : "#252525",
-    //   };
-    // },
   },
   methods: {
     toggleFol() {
-      this.isFol = !this.isFol;
-      console.log(this.isFol);
+      //歌單追蹤功能--------------------------
+      if (this.functype === 0) {
+        if (this.isFol) {
+          this.isFol = !this.isFol;
+          //測試用，以下撰寫功能
+          alert("取消追蹤歌單");
+        } else {
+          this.isFol = !this.isFol;
+          //測試用，以下撰寫功能
+          alert("追蹤歌單");
+        }
+      }
+      //創作者追蹤功能--------------------------
+      else if (this.functype === 1) {
+        if (this.isFol) {
+          this.isFol = !this.isFol;
+          //測試用，以下撰寫功能
+          alert("取消追蹤創作者");
+        } else {
+          this.isFol = !this.isFol;
+          //測試用，以下撰寫功能
+          alert("追蹤創作者");
+        }
+      }
+      //音樂快訊追蹤功能--------------------------
+      else if (this.functype === 2) {
+        if (this.isFol) {
+          this.isFol = !this.isFol;
+          //測試用，以下撰寫功能
+          alert("取消追蹤音樂快訊");
+        } else {
+          this.isFol = !this.isFol;
+          //測試用，以下撰寫功能
+          alert("追蹤音樂快訊");
+        }
+      }
     },
   },
 };
@@ -35,15 +65,12 @@ export default {
 
 <style scoped lang="scss">
 #FolBtnBig {
-  @include btnColor($green, false);
-  color: #ffffff;
-  font-size: $default;
-  i {
-    display: none;
-  }
+  @include btnColor($green, false, false);
+  font-size: 14px;
+  padding: 4px 13px;
 }
 #FolBtnBig.active {
-  @include btnColor($green);
+  @include btnColor($green, true, false);
   background-color: transparent;
 }
 </style>
