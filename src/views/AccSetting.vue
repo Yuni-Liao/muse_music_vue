7e781893b9f5f30ce8047cf97274e34fb28daa04
Yuni-Liao <yuni70217@gmail.com>
2023-09-14 16:47:12 +0800

    新增 個人帳號設定按鈕 script

--- src/views/AccSetting.vue
+++ src/views/AccSetting.vue
@@ -28,34 +28,13 @@
                         <p>國家或地區</p>
                         <input class="nation_box obj_Radius" type="text" readonly placeholder="臺灣">
                         <select class="city obj_Radius" id="city">
-                            <option value="台北市">台北市</option>
-                            <option value="新北市">新北市</option>
-                            <option value="基隆市">基隆市</option>
-                            <option value="桃園市">桃園市</option>
-                            <option value="新竹市">新竹市</option>
-                            <option value="新竹縣">新竹縣</option>
-                            <option value="苗栗縣">苗栗縣</option>
-                            <option value="臺中市">臺中市</option>
-                            <option value="彰化縣">彰化縣</option>
-                            <option value="南投縣">南投縣</option>
-                            <option value="雲林縣">雲林縣</option>
-                            <option value="嘉義縣">嘉義縣</option>
-                            <option value="嘉義市">嘉義市</option>
-                            <option value="臺南市">臺南市</option>
-                            <option value="高雄市">高雄市</option>
-                            <option value="屏東縣">屏東縣</option>
-                            <option value="宜蘭縣">宜蘭縣</option>
-                            <option value="花蓮縣">花蓮縣</option>
-                            <option value="臺東縣">臺東縣</option>
-                            <option value="澎湖縣">澎湖縣</option>
-                            <option value="金門縣">金門縣</option>
-                            <option value="連江縣">連江縣</option>
+                            <option v-for="(item, index) in cityName" :key="index" value="{{ item }}">{{ item }}</option>
                         </select>
                     </div>
                 </div>
             </div>
             <div class="btn">
-                <button class="acc_savebtn default_Btn obj_Rounded btn_XS_NoBorder">儲存變更</button>
+                <button @click="saveBtn()" class="acc_savebtn default_Btn obj_Rounded btn_XS_NoBorder">儲存變更</button>
             </div>
         </div>
     </main>
