fa321a5688391f7ecc355bb672f0e11af98338d2
rou94980 <rou94980@gmail.com>
2023-09-16 15:45:08 +0800

    部屬小龜幫幫忙

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   css: {
@@ -6,8 +6,21 @@ module.exports = defineConfig({
       scss: {
         additionalData: `
           @import "~@/assets/scss/style.scss";
-        `
+        `,
       },
-    }
+    },
   },
-})
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "https://tibamef2e.com/chd103/g2/api/",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": "",
+        },
+      },
+    },
+  },
+  publicPath: process.env.NODE_ENV === "production" ? "/chd103/g2/" : "/",
+  // outputDir: "muse_music",
+});
