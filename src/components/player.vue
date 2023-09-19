d5ac0a6ef6d4c11f7576e1ec8c7f9441c7801fde
karta1020146 <karta1020146@gmail.com>
2023-09-19 17:02:25 +0800

    update player_currentTime

--- src/components/player.vue
+++ src/components/player.vue
@@ -22,15 +22,11 @@
                 <div id="play">
                     <!-- <audio id="myAudio" ref="music" src="https://yildirimzlm.s3.us-east-2.amazonaws.com/Post+Malone+-+rockstar+ft.+21+Savage+(Official+Audio).mp3"></audio> -->
                     <audio id="myAudio" ref="music" src="audio/Busy Day Ahead.mp3" @timeupdate="updateTime"></audio>
-                    <fontAwesome :icon="['fa', 'play']" size="2xl" style="color: #fff; margin: 15px; cursor: pointer;" 
-                        v-if="!isPlaying"
-                        @click="playMusic"
-                    />
-                    <fontAwesome :icon="['fa', 'pause']" size="2xl" style="color: #fff; margin: 15px; cursor: pointer;" 
-                        v-else
-                        @click="pauseMusic"
-                    />
-                   
+                    <fontAwesome :icon="['fa', 'play']" size="2xl" style="color: #fff; margin: 15px; cursor: pointer;"
+                        v-if="!isPlaying" @click="playMusic" />
+                    <fontAwesome :icon="['fa', 'pause']" size="2xl" style="color: #fff; margin: 15px; cursor: pointer;"
+                        v-else @click="pauseMusic" />
+
                 </div>
                 <div id="next" class="forwargBtn controlsItem">
                     <fontAwesome :icon="['fa', 'fa-step-forward']" size="2xl"
@@ -53,7 +49,6 @@
             <p><span>{{ volume }}</span></p>
         </div>
     </div>
-
 
     <!-- -------------蓋板播放器範圍------------- -->
     <div class="modal" v-if="isModalVisible" :style="{
@@ -87,7 +82,8 @@
                     <h5>George Makridis</h5>
                     <div class="timeBarr">
                         <p>{{ formatTime(currentTime) }}</p>
-                        <input class="input-range--custom" type="range" v-model="currentTime" @input="seekToTime" step="0.01" >
+                        <input class="input-range--custom" type="range" v-model="currentTime" @input="seekToTime"
+                            step="0.01">
                         <p>{{ formatTime(totalTime) }}</p>
                     </div>
                     <div class="fcbtns">
@@ -192,7 +188,6 @@ export default {
             showPlayer: false,
             currentTime: 0,
             totalTime: 0,
-
         }
     },
     methods: {
@@ -200,22 +195,19 @@ export default {
         // return require("audio/" + src)
         // },
         playMusic() {
-        //播放音樂
-        // 播放音樂
-        this.playerOpen = true;//先執行顯示
+            //播放音樂
+            // 播放音樂
+            this.playerOpen = true;//先執行顯示
             this.$nextTick(() => {//再執行播放
                 this.$refs.music.play();
                 this.isPlaying = true;
-                
             });
-
         },
         pauseMusic() {
             // 暫停音樂
             this.$refs.music.pause();
             this.isPlaying = false;
         },
-
         toggleMute() {
             if (this.isMuted) {
                 this.$refs.music.volume = this.currentVolume;
@@ -239,13 +231,14 @@ export default {
             audio.currentTime = this.currentTime;
         },
         updateTime() {
-        // 在音樂時間更新時觸發，用於更新currentTime
-        const audioElement = this.$refs.music;
-        this.currentTime = audioElement.currentTime;
-        this.totalTime = audioElement.duration;
+            // 在音樂時間更新時觸發，用於更新currentTime
+            const audioElement = this.$refs.music;
+            if (audioElement) {
+                this.currentTime = audioElement.currentTime;
+                this.totalTime = audioElement.duration;
+            }
         },
     },
-    
     computed: {
         formatTime() {
             const format = (time) => {
@@ -257,13 +250,11 @@ export default {
         }
     },
     watch: {
-                volume: function(newVolume) {
-                    const audioElement = document.getElementById("myAudio");
-                    audioElement.volume = newVolume / 100;
-                },
-                
-                
-            }
+        volume: function (newVolume) {
+            const audioElement = document.getElementById("myAudio");
+            audioElement.volume = newVolume / 100;
+        },
+    }
 };
 </script>
 
