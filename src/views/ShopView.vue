046f87cb8bf5d07612e9c3f3ef74aecc119aad5b
Yuni-Liao <yuni70217@gmail.com>
2023-09-21 23:45:04 +0800

    adjust router-link

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -34,13 +34,14 @@
         <div class="inner">
             <div class="prod">
                 <div class="prodCard" v-for="(item, id) in products" :key="id">
-                    <router-link :to="`/shopproddetail/${id}`"  class="prodPic">
+                    <router-link :to="`/home/shopproddetail/${id}`" class="prodPic">
                         <!-- <router-link to="/shopproddetail/:id" class="prodPic" @click="goToProductDetail(id)"></router-link> -->
                         <!-- <img :src="item.prodPic" :alt="item.prodName"> -->
                         <img :src="require(`/public/image/ShopImage/${item.prodPic}`)" :alt="item.prodName">
                     </router-link>
                     <div class="prodTxt">
-                        <router-link to="`/shopproddetail/${item.id}`" class="prodName">{{ item.prodName }}</router-link>
+                        <router-link to="`/home/shopproddetail/${item.id}`" class="prodName">{{ item.prodName
+                        }}</router-link>
                         <p class="prodPrice">$ {{ item.prodPrice }}</p>
 
                         <!-- 數量加減 -->
@@ -63,7 +64,7 @@
             <Page :total="catList.length" show-sizer :page-size-opts="[20, 16, 12, 8]" :page-size="selectedPageSize"
                 @on-change="handlePageChange" @on-page-size-change="handlePageSize" />
         </div>
-        <router-link to="/shoppingsteps">
+        <router-link to="/home/shoppingsteps">
             <button class="shopcart">
                 <img src="/image/icon/shopcart.png" alt="shopcart">
             </button>
