4bb4ad9e0053db644e49d526ae6a206ed5484958
Yuni-Liao <yuni70217@gmail.com>
2023-09-27 16:01:09 +0800

    先隱藏未製作的功能

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,43 +1,43 @@
 <template>
-    <div class="phone-show">
-      <router-link to="/home/find" style="margin: 0px 10px;">
-        <fontAwesome :icon="['fa', 'fa-compass']" size="xl" style="color: #fff; cursor: pointer;" />
-        <p>探索</p>
-      </router-link>
-      <router-link to="/home/ranking" style="margin: 0px 10px;">
-        <fontAwesome :icon="['fa', 'fa-arrow-trend-up']" size="xl" style="color: #fff; cursor: pointer;" />
-        <p>排行榜</p>
-      </router-link>
-      <router-link to="/home/shop" style="margin: 0px 10px;">
-        <fontAwesome :icon="['fa', 'fa-bag-shopping']" size="xl" style="color: #fff; cursor: pointer;" />
-        <p>周邊販售</p>
-      </router-link>
-      <router-link to="/home/activity" style="margin: 0px 10px;">
-        <fontAwesome :icon="['fa', 'fa-bolt']" size="xl" style="color: #fff; cursor: pointer;" />
-        <p>音樂快訊</p>
+  <div class="phone-show">
+    <router-link to="/home/find" style="margin: 0px 10px;">
+      <fontAwesome :icon="['fa', 'fa-compass']" size="xl" style="color: #fff; cursor: pointer;" />
+      <p>探索</p>
+    </router-link>
+    <router-link to="/home/ranking" style="margin: 0px 10px;">
+      <fontAwesome :icon="['fa', 'fa-arrow-trend-up']" size="xl" style="color: #fff; cursor: pointer;" />
+      <p>排行榜</p>
+    </router-link>
+    <router-link to="/home/shop" style="margin: 0px 10px;">
+      <fontAwesome :icon="['fa', 'fa-bag-shopping']" size="xl" style="color: #fff; cursor: pointer;" />
+      <p>周邊販售</p>
+    </router-link>
+    <router-link to="/home/activity" style="margin: 0px 10px;">
+      <fontAwesome :icon="['fa', 'fa-bolt']" size="xl" style="color: #fff; cursor: pointer;" />
+      <p>音樂快訊</p>
+    </router-link>
+  </div>
+  <footer :style="{
+    backgroundImage: `url( ${require('@/assets/image/footer_background.jpg')} )`,
+    backgroundSize: '100% auto',
+  }">
+    <div class="logo_footer">
+      <router-link to="/home">
+        <img alt="Vue logo" src="~@/assets/image/muse_logo_2.png" />
       </router-link>
     </div>
-    <footer :style="{
-        backgroundImage: `url( ${require('@/assets/image/footer_background.jpg')} )`,
-        backgroundSize: '100% auto',
-    }">
-        <div class="logo_footer">
-            <router-link to="/home">
-                <img alt="Vue logo" src="~@/assets/image/muse_logo_2.png" />
-            </router-link>
-        </div>
-        <div class="footer_nav">
-            <router-link to="/home/about">關於謬思</router-link> |
-            <router-link to="/home/activity">音樂快訊</router-link> |
-            <router-link to="#">常見問題</router-link> |
-            <router-link to="#">隱私權政策</router-link>
-        </div>
-        <p class="tibame">
-            本網站為緯育TibaMe_前端設計工程師班第86期學員專題成果作品,本平台僅供學習、展示之用。<br />
-            若有抵觸有關著作權,或有第三人主張侵害智慧財產權等情事,均由學員負法律上責任,<br />
-            緯育公司概不負責。若有侵權疑慮,您可以私訊[緯育TibaMe],後續會由專人協助處理。
-        </p>
-    </footer>
+    <div class="footer_nav">
+      <router-link to="/home/about">關於謬思</router-link> |
+      <router-link to="/home/activity">音樂快訊</router-link> |
+      <router-link to="#">常見問題</router-link> |
+      <router-link to="#">隱私權政策</router-link>
+    </div>
+    <p class="tibame">
+      本網站為緯育TibaMe_前端設計工程師班第86期學員專題成果作品,本平台僅供學習、展示之用。<br />
+      若有抵觸有關著作權,或有第三人主張侵害智慧財產權等情事,均由學員負法律上責任,<br />
+      緯育公司概不負責。若有侵權疑慮,您可以私訊[緯育TibaMe],後續會由專人協助處理。
+    </p>
+  </footer>
 </template>
 
 <style scoped lang="scss">
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,11 @@
 <template>
-  <header >
+  <header>
     <!-- 非v-bind的圖片載入方式 -->
     <!-- 注意:: 點擊logo>返回首頁的跳轉要用router-link to="___" 這方式套才不會404, 用<a>沒辦法 -->
     <nav headroom headroom--unpinned id="navbar">
       <div class="header_nav_left">
         <router-link to="/home">
-          <img
-            class="logo_header"
-            alt="Vue logo"
-            src="~@/assets/image/muse_logo.png"
-          />
+          <img class="logo_header" alt="Vue logo" src="~@/assets/image/muse_logo.png" />
         </router-link>
         <router-link to="/home/find" class="find">探索</router-link>
         <router-link to="/home/ranking" class="ranking">排行榜</router-link>
@@ -25,78 +21,41 @@
           <img alt="search_icon" src="~@/assets/image/icon/search.png" />
         </div>
         <div class="notify_dropdown_reaction">
-          
+
           <img :alt="'bell_icon'" src="~@/assets/image/icon/bell.png" />
 
           <div class="notify_dropdown">
             <p>通知中心</p>
             <hr />
-            <div
-              class="notify"
-              v-for="(item, index) in notifyList"
-              :key="index"
-            >
-              <img
-                :src="require(`@/assets/image/creator/${item.src}`)"
-                alt=""
-              />
+            <div class="notify" v-for="(item, index) in notifyList" :key="index">
+              <img :src="require(`@/assets/image/creator/${item.src}`)" alt="" />
               <p>{{ item.notifytxt }}</p>
             </div>
           </div>
         </div>
         <div class="user_dropdown_reaction">
-          <router-link to="/home/login"
-            ><img
-              style="margin-top: -1px"
-              alt="user_icon"
-              src="~@/assets/image/icon/user.png"
-          /></router-link>
+          <router-link to="/home/login"><img style="margin-top: -1px" alt="user_icon"
+              src="~@/assets/image/icon/user.png" /></router-link>
 
           <div class="user_dropdown">
-            <router-link to=""
-              ><img
-                src="~@/assets/image/icon/clipboard.png"
-                alt=""
-              />訂單資訊</router-link
-            ><br />
-            <router-link to="/home/accsetting"
-              ><img
-                src="~@/assets/image/icon/settingicon.png"
-                alt=""
-              />帳號設定</router-link
-            ><br />
-            <router-link to="/home/profilepage"
-              ><img
-                src="~@/assets/image/icon/personalPage.png"
-                alt=""
-              />個人主頁</router-link
-            ><br />
-            <router-link to="/home/profilepageedit"
-              ><img
-                src="~@/assets/image/icon/development.png"
-                alt=""
-              />個人管理頁面</router-link
-            ><br />
-            <router-link to="/home/shoppingsteps"
-              ><img
-                src="~@/assets/image/icon/development.png"
-                alt=""
-              />購物車流程</router-link
-            ><br />
+            <router-link to=""><img src="~@/assets/image/icon/clipboard.png" alt="" />訂單資訊</router-link><br />
+            <router-link to="/home/accsetting"><img src="~@/assets/image/icon/settingicon.png"
+                alt="" />帳號設定</router-link><br />
+            <router-link to="/home/profilepage"><img src="~@/assets/image/icon/personalPage.png"
+                alt="" />個人主頁</router-link><br />
+            <router-link to="/home/profilepageedit"><img src="~@/assets/image/icon/development.png"
+                alt="" />個人主頁管理</router-link><br />
+            <router-link to="/home/shoppingsteps"><img src="~@/assets/image/icon/development.png"
+                alt="" />購物車流程</router-link><br />
             <p>我的音樂庫</p>
-            <router-link to="/home"
+            <!-- <router-link to="/home"
               ><img
                 src="~@/assets/image/icon/clock.png"
                 alt=""
               />播放紀錄</router-link
-            ><br />
-            <router-link to="/home/mysonglist"
-              ><img
-                src="~@/assets/image/icon/list.png"
-                alt=""
-              />我的歌單</router-link
-            ><br />
-            <router-link to="/home"
+            ><br /> -->
+            <router-link to="/home/mysonglist"><img src="~@/assets/image/icon/list.png" alt="" />我的歌單</router-link><br />
+            <!-- <router-link to="/home"
               ><img
                 src="~@/assets/image/icon/heart.png"
                 alt=""
@@ -107,62 +66,65 @@
                 src="~@/assets/image/icon/targeticon.png"
                 alt=""
               />我的追蹤</router-link
-            >
+            > -->
             <hr style="margin: 10px 0px" />
             <div class="loginbtn">登出</div>
           </div>
         </div>
         <div class="hamburger" @click="isNavVisible = !isNavVisible">
-          <img  alt="hamburger_icon" src="~@/assets/image/icon/hamburger.svg"/>
+          <img alt="hamburger_icon" src="~@/assets/image/icon/hamburger.svg" />
         </div>
       </div>
     </nav>
     <div class="phone-show-nav" v-show="isNavVisible">
       <div class="headshot">
         <img src="@/assets/image/profileeditimage/profileimage.jpg" alt="">
-        <fontAwesome @click="isNavVisible = !isNavVisible" class="close" :icon="['fa', 'xmark']" size="2xl" style="color: #fff;" />
+        <fontAwesome @click="isNavVisible = !isNavVisible" class="close" :icon="['fa', 'xmark']" size="2xl"
+          style="color: #fff;" />
       </div>
       <router-link to="#" @click="isNavVisible = false">
-        <img src="~@/assets/image/icon/clipboard.png" alt=""/>
+        <img src="~@/assets/image/icon/clipboard.png" alt="" />
         <p>訂單資訊</p>
       </router-link>
       <router-link to="/home/accsetting" @click="isNavVisible = false">
-        <img src="~@/assets/image/icon/settingicon.png" alt=""/>
+        <img src="~@/assets/image/icon/settingicon.png" alt="" />
         <p>帳號設定</p>
       </router-link>
       <router-link to="/home/profilepage" @click="isNavVisible = false">
-        <img src="~@/assets/image/icon/personalPage.png" alt=""/>
+        <img src="~@/assets/image/icon/personalPage.png" alt="" />
         <p>個人主頁</p>
       </router-link>
       <router-link to="/home/profilepageedit" @click="isNavVisible = false">
-        <img src="~@/assets/image/icon/development.png" alt=""/>
+        <img src="~@/assets/image/icon/development.png" alt="" />
         <p>個人管理頁面</p>
       </router-link>
-      
+
       <p style="margin: 30px 0px 20px 25px; font-size: 20px;">我的音樂庫</p>
-            
+
       <router-link to="/home" @click="isNavVisible = false">
-        <img src="~@/assets/image/icon/clock.png" alt=""/>
+        <img src="~@/assets/image/icon/clock.png" alt="" />
         <p>播放紀錄</p>
       </router-link>
       <router-link to="/home/mysonglist" @click="isNavVisible = false">
-        <img src="~@/assets/image/icon/list.png" alt=""/>
+        <img src="~@/assets/image/icon/list.png" alt="" />
         <p>我的歌單</p>
       </router-link>
       <router-link to="/home" @click="isNavVisible = false">
-        <img src="~@/assets/image/icon/heart.png" alt=""/>
+        <img src="~@/assets/image/icon/heart.png" alt="" />
         <p>我的最愛</p>
       </router-link>
       <router-link to="/home" @click="isNavVisible = false">
-        <img src="~@/assets/image/icon/targeticon.png" alt=""/>
+        <img src="~@/assets/image/icon/targeticon.png" alt="" />
         <p>我的追蹤</p>
       </router-link>
-      
-      
-      <div class="loginbtn"><p style="margin: 30px 0px 10px 25px; font-size: 20px;">登出</p></div>
+
+
+      <div class="loginbtn">
+        <p style="margin: 30px 0px 10px 25px; font-size: 20px;">登出</p>
+      </div>
 
     </div>
-    
+
   </header>
 </template>
 
@@ -205,7 +167,7 @@ export default {
     });
   },
   methods: {
-    
+
   },
 };
 </script>
