56a77941e6a4e091677bf9cd3c59c05cdc989c61
rou94980 <rou94980@gmail.com>
2023-10-05 17:33:59 +0800

    feat 歌單頁面 加入歌單功能

--- src/views/js/SingleSonglistView.js
+++ src/views/js/SingleSonglistView.js
@@ -2,50 +2,22 @@ import PlayBtnBig from "@/components/PlayBtnBig.vue";
 import ShareBtn from "@/components/ShareBtn.vue";
 import FolBtnBig from "@/components/FolBtnBig.vue";
 import player from "@/components/player.vue";
+import AddSlBtn from "@/components/AddSlBtn.vue";
 
 export default {
-  components: { PlayBtnBig, ShareBtn, FolBtnBig, player },
+  components: { PlayBtnBig, ShareBtn, FolBtnBig, player, AddSlBtn },
   data() {
     return {
       // 讓圖片 build 之後能顯示
       publicPath: process.env.BASE_URL,
       //more 顯示狀態
       morecurrent: -1,
+      //isAddSlOpen: false,
       songlist: [], //歌單資訊 (fetch)
       slSongs: [], //歌單歌曲 (fetch)
     };
   },
   methods: {
-    // 獲取歌單資訊
-    fetchSonglistDetail() {
-      const slid = this.$route.params.slid;
-      const apiURL = new URL(
-        `http://localhost/muse_music/public/api/getSonglistDetail.php?slid=${slid}`
-      );
-      fetch(apiURL)
-        .then(async (response) => {
-          this.songlist = await response.json();
-        })
-        .catch((error) => {
-          console.error("發生錯誤:", error);
-        });
-    },
-
-    // 獲取歌單歌曲資訊
-    fetchSonglistSong() {
-      const slid = this.$route.params.slid;
-      const apiURL = new URL(
-        `http://localhost/muse_music/public/api/getSonglistSong.php?slid=${slid}`
-      );
-      fetch(apiURL)
-        .then(async (response) => {
-          this.slSongs = await response.json();
-        })
-        .catch((error) => {
-          console.error("發生錯誤:", error);
-        });
-    },
-
     //切換 more 開啟
     showtoggle(e, index) {
       // console.log(e.target.nextElementSibling);
@@ -68,44 +40,77 @@ export default {
     addFav(e) {
       alert(`歌曲ID${e}，加入我的最愛`);
     },
-    addSonglist(e) {
-      alert(`歌曲ID${e}，加入我的歌單`);
+    addSonglist(index) {
+      // alert(`歌曲ID${e}，加入我的歌單`);
+      // this.isAddSlOpen = true;
+      // console.log(this.isAddSlOpen);
+      this.$refs.AddSl[index].openAddSl();
+      //console.log(this.$refs.AddSl[index]);
     },
     //頁面切換--------------
     gotosinglemusic(sid) {
       this.$router.push({
         name: "singlemusic",
-        query: {
-          q: sid,
+        params: {
+          sid,
         },
       });
     },
     gotosinger(memid) {
       this.$router.push({
         name: "profilepage",
-        query: {
-          q: memid,
+        params: {
+          memid,
         },
       });
     },
     gotosinglealbum(abid) {
       this.$router.push({
         name: "singlealbum",
-        query: {
-          q: abid,
+        params: {
+          abid,
         },
       });
     },
   },
   mounted() {
+    this.pageslid = parseInt(this.$route.params.slid);
+    // fetch歌單資訊
+    const fetchSonglistDetail = () => {
+      const slid = this.$route.params.slid;
+      const apiURL = new URL(
+        `http://localhost/muse_music/public/api/getSonglistDetail.php?slid=${slid}`
+      );
+      fetch(apiURL)
+        .then(async (response) => {
+          this.songlist = await response.json();
+        })
+        .catch((error) => {
+          console.error("發生錯誤:", error);
+        });
+    };
+
+    // fetch歌單歌曲資訊
+    const fetchSonglistSong = () => {
+      const slid = this.$route.params.slid;
+      const apiURL = new URL(
+        `http://localhost/muse_music/public/api/getSonglistSong.php?slid=${slid}`
+      );
+      fetch(apiURL)
+        .then(async (response) => {
+          this.slSongs = await response.json();
+        })
+        .catch((error) => {
+          console.error("發生錯誤:", error);
+        });
+    };
+
+    //執行fetch
+    fetchSonglistDetail();
+    fetchSonglistSong();
+
     //建立事件聆聽:點空白處關閉
     document.addEventListener("click", this.closemore, true);
-    this.pageslid = parseInt(this.$route.params.slid);
-
-    // 獲取歌單資訊
-    this.fetchSonglistDetail();
-    // 獲取歌單歌曲資訊
-    this.fetchSonglistSong();
   },
   beforeUnmount() {
     //移除事件聆聽:點空白處關閉
