d4cfcf3bedc5ab5a8a25078c3ecd15d632e07276
kay <candy168741@gmail.com>
2023-10-06 03:16:08 +0800

    update 單曲頁面php

--- public/api/getSingleAlbum.php
+++ public/api/getSingleAlbum.php
@@ -1,4 +1,3 @@
-<!-- 專輯頁面：抓取該張專輯的資料 -->
 <?php
 try {
     //引入連線工作的檔案
@@ -11,19 +10,17 @@ try {
     $salid = 1;
     //SQL指令: 查詢專輯內容
     $sql = "select 
-    al.alb_id as id,
-    al.alb_name as albumName,
-    al.alb_intro as albumInf,
-    al.alb_img as albumPic,
+    al.alb_id,
+    al.alb_name as albumname,
+    al.alb_intro as albuminf,
+    al.alb_img as albumpic,
     al.mem_id as singer_id,
     al.share_num as shared,
     al.upload_date as date,
     m.mem_name as singer,
-    m.mem_pic as singerPic,
-    s.s_id
+    m.mem_pic as singerpic
     from album al
     join member m on al.mem_id = m.mem_id
-    join song s on s.alb_id = al.alb_id
     where al.alb_id = $salid;";
 
     $singleAlbum = $pdo->query($sql);
@@ -36,6 +33,5 @@ try {
     }
 } catch (Exception $e) {
     echo "錯誤行號 : ", $e->getLine(), "<br>";
-    echo "錯誤原因 : ", $e->getMessage(), "<br>";
-    //echo "系統暫時不能正常運行，請稍後再試<br>";	
+    echo "錯誤原因 : ", $e->getMessage(), "<br>";	
 }
