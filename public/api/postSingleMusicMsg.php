4e8f6743ea3579674290ec1697b12ff93abfa62d
kay <candy168741@gmail.com>
2023-10-09 01:59:34 +0800

    update 留言還是失敗中QQ

--- public/api/postSingleMusicMsg.php
+++ public/api/postSingleMusicMsg.php
@@ -6,30 +6,40 @@ try {
     header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
     header("Content-Type: application/json");
 
-    //取得單曲頁面 新留言内容
-    $newMessage = $_POST['message'];
+    // 取得前端發送的JSON數據
+    $json_data = file_get_contents('php://input');
+    $data = json_decode($json_data);
 
-    // SQL新增  回傳到msg表格
-    $sql = "INSERT INTO msg (msg_id, mem_id, s_id, msg_con,msg_date, msg_like) VALUES (:msg_id, :mem_id, :s_id, :msg_con, :msg_date, :msg_like)";
+    if ($data === null) {
+        // 如果JSON解析失敗
+        echo json_encode(["success" => false, "msg" => "无效的JSON数据"]);
+        exit;
+    }
+
+    // 取得單曲頁面新留言内容
+    $newMessage = $data->message;
+
+    // SQL新增回傳到msg表格
+    $sql = "INSERT INTO msg (mem_id, s_id, msg_con, msg_date, msg_like) VALUES (:mem_id, :s_id, :msg_con, :msg_date, :msg_like)";
     $stmt = $pdo->prepare($sql);
 
-    $stmt->bindParam(':msg_id', $msg_id, PDO::PARAM_INT);
     $stmt->bindParam(':mem_id', $mem_id, PDO::PARAM_INT);
     $stmt->bindParam(':s_id', $s_id, PDO::PARAM_INT);
-    $stmt->bindParam(':msg_con', $newMessage, PDO::PARAM_STR);
-    $stmt->bindParam(':msg_date', $msg_date, PDO::PARAM_STR);
-    $stmt->bindParam(':msg_like', $msg_like, PDO::PARAM_STR);
+    $stmt->bindParam(':msg_con', $newMessage->message, PDO::PARAM_STR);
+    $stmt->bindParam(':msg_date', $newMessage->date, PDO::PARAM_STR);
+    $stmt->bindParam(':msg_like', $newMessage->like, PDO::PARAM_INT);
 
     $mem_id = 1; // 先假設 mem_id 是 1
     $s_id = 1; // 先假設 s_id 是 1
 
     if ($stmt->execute()) {
-        echo json_encode(["success" => true, "msg" => "留言成功"]);
+        // 返回成功和新留言对象
+        echo json_encode(["success" => true, "message" => $newMessage]);
     } else {
-        echo json_encode(["success" => false, "msg" => "留言失敗"]);
+        echo json_encode(["success" => false, "msg" => "留言失败"]);
     }
 } catch (Exception $e) {
-    // 返回 JSON 錯誤響應
-    $errorResponse = ["success" => false, "msg" => "留言發生錯誤：" . $e->getMessage()];
+    $errorResponse = ["success" => false, "msg" => "留言发生错误：" . $e->getMessage()];
     echo json_encode($errorResponse);
 }
+?>
