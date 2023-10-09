9d858cdc7f49c20e541c4b4d6fd20b19370c91eb
rou94980 <rou94980@gmail.com>
2023-10-10 04:54:35 +0800

    feat php 錯誤訊息

--- public/api/getMyCreateSonglists.php
+++ public/api/getMyCreateSonglists.php
@@ -31,7 +31,14 @@ try {
 
     echo json_encode($result);
 } catch (Exception $e) {
-    echo "錯誤行號 : ", $e->getLine(), "<br>";
-    echo "錯誤原因 : ", $e->getMessage(), "<br>";
+    $errorResponse = [
+        "error" => [
+            "message" => "新增失敗",
+            "line" => $e->getLine(),
+            "details" => $e->getMessage(),
+        ],
+    ];
+
+    echo json_encode($errorResponse);
 }
 ?>
