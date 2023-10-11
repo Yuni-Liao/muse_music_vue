d2c81e312efc2f73b5d3d3d4fff70b7a7f252ee2
ZXC559231 <zxc559231@gmail.com>
2023-10-11 10:05:07 +0800

    add order.php

---
+++ public/api/getShopOrders.php
@@ -0,0 +1,27 @@
+<?php
+try{
+    //引入連線工作的檔案
+    header('Access-Control-Allow-Origin:*');
+    header("Content-Type:application/json;charset=utf-8");
+    require_once("./connectMusemusic.php");
+
+    //執行sql指令並取得pdoStatement
+    //SQL指令: 查詢orders
+    $sql = "select * from orders 
+    order by ord_id desc;";
+
+    $orders = $pdo->query($sql);
+
+    //如果找得資料，取回資料，送出json
+    if ($orders->rowCount() === 0) {
+        echo "查無資料";
+    } else {
+        $orderRow = $orders->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($orderRow); //送出json字串
+    }
+
+} catch (Exception $e) {
+    echo "錯誤行號 : ", $e->getLine(), "<br>";
+    echo "錯誤原因 : ", $e->getMessage(), "<br>";
+}
+?>
