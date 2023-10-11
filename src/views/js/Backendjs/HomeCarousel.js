80c6173f506aef94c3758610f4c372e58d69edf3
Yuni-Liao <yuni70217@gmail.com>
2023-10-11 14:24:36 +0800

    後台 首頁輪播圖 編輯功能 完成

--- src/views/js/Backendjs/HomeCarousel.js
+++ src/views/js/Backendjs/HomeCarousel.js
@@ -6,7 +6,7 @@ export default {
             //
             // bannerBox: false, //新增輪播按鈕先隱藏-yuni
             editBox: false,
-            uploadedImg: null, // 上傳圖片暫存
+            // uploadedImg: '', // 上傳圖片暫存
             carouselItem: [], // 渲染資料的暫存陣列
             columns: [
                 {
@@ -54,10 +54,11 @@ export default {
             ],
             editItem: {
                 car_id: '',
-                nameValue: '',
-                bannerLink: '',
-                // uploadImg: '',
+                name: '',
+                link: '',
+                img: '',
             },
+            selectedCarRank: '',
         }
     },
     methods: {
@@ -67,103 +68,58 @@ export default {
         upDownBtn(row) {
             alert('上/下架');
         },
-        // 圖片還沒寫進DB 壞的~~ 
-        imgChange(file) {
-            console.log('文件名稱:', file.name);
-            console.log('文件大小:', file.size);
+        imgChange(e) {
+            let that = this;
+            let files = e.target.files[0];
+            if (!e || !window.FileReader) return;
+            let reader = new FileReader();
+            reader.readAsDataURL(files);
 
-            console.log('uploadedImg 值:', this.uploadedImg);
-
-            const reader = new FileReader();
-            reader.onload = (e) => {
-                this.uploadedImg = e.target.result;
-                console.log('上傳後的 uploadedImg 值:', this.uploadedImg);
+            reader.onloadend = function () {
+                that.editItem.img = files;
             };
-
-            reader.onerror = (error) => {
-                console.error('上傳失敗:', error);
-            };
-
-            reader.readAsDataURL(file);
         },
         editCarousel(row) {
             this.editBox = true;
             this.editItem.car_id = row.car_id;
-            this.editItem.nameValue = row.nameValue;
-            this.editItem.bannerLink = row.bannerLink;
-            // this.editItem.uploadImg = row.uploadImg;
+            this.editItem.name = row.name;
+            this.editItem.link = row.link;
+            this.editItem.img = row.img;
         },
-        confirmEdit() {
-            const formData = new FormData();
+
+        saveBtn() {
             const url = `http://localhost/muse_music/public/api/editIndexCarousel.php`;
-            let headers = {
-                Accept: "application/json",
-            };
-            formData.append('car_id', this.editItem.car_id);
-            formData.append('name', this.editItem.nameValue);
-            formData.append('link', this.editItem.bannerLink);
-            // formData.append('img', tthis.uploadedImg);
-            if (this.uploadedImg) {
-                formData.append('img', this.uploadedImg);
-            }
+
+            const formData = new FormData();
+            formData.append("car_id", this.editItem.car_id);
+            formData.append("name", this.editItem.name);
+            formData.append("link", this.editItem.link);
+            formData.append("img", document.getElementById("fileImg").files[0]);
+            console.log(document.getElementById("fileImg").files[0]);
+
             fetch(url, {
                 method: "POST",
-                headers: headers,
                 body: formData,
             })
                 .then((response) => {
                     if (response.ok) {
+                        console.log(response);
                         return response.json();
                     } else {
                         throw new Error("新增失敗");
                     }
                 })
                 .then((json) => {
-                    // alert(json);
-                    console.log(formData.get('car_id'));
-                    console.log(formData.get('name'));
-                    console.log(formData.get('link'));
-                    console.log(formData.get('img'));
-                    console.log(JSON.stringify(json));
-                    // window.location.reload();
+                    alert(json);
+                    window.location.reload();
                 })
                 .catch((error) => {
                     console.log(error.message);
                 });
-
-            // const url = `http://localhost/muse_music/public/api/editIndexCarousel.php`;
-            // let headers = {
-            //     "Content-Type": "application/json",
-            //     Accept: "application/json",
-            // };
-            // const dataToSend = {
-            //     car_id: this.editItem.car_id,
-            //     name: this.editItem.nameValue,
-            //     link: this.editItem.bannerLink,
-            //     img: this.editItem.uploadImg,
-            // };
-            // console.log(this.editItem.uploadImg)
-            // fetch(url, {
-            //     method: "POST",
-            //     headers: headers,
-            //     body: JSON.stringify(dataToSend),
-            // })
-            // .then((response) => {
-            //     if (response.ok) {
-            //         return response.json();
-            //     } else {
-            //         throw new Error("新增失敗");
-            //     }
-            // })
-            // .then((json) => {
-            //     alert(json);
-            //     window.location.reload();
-            // })
-            // .catch((error) => {
-            //     console.log(error.message);
-            // });
-            // console.log(dataToSend);
         },
+        closeBtn() {
+            this.editBox = false;
+        }
     },
     mounted() {
         //先檢查資料格式是否符合DB規則
