c86f58ced32c225b56e5a7e73dcd2617b76377fc
kay <candy168741@gmail.com>
2023-10-13 23:23:47 +0800

    add 後台 商品管理 上傳圖片可以預覽 & 壓縮商城圖片

--- src/views/js/Backendjs/ProdMgmt.js
+++ src/views/js/Backendjs/ProdMgmt.js
@@ -86,7 +86,7 @@ export default {
             alert('新增商品');
         },
         //變更圖片
-        imgChange(e) {
+        img(e) {
             let that = this;
             let files = e.target.files[0];
             if (!e || !window.FileReader) return;
@@ -94,7 +94,7 @@ export default {
             reader.readAsDataURL(files);
 
             reader.onloadend = function () {
-                that.editItem.prod_pic = files;
+                that.editItem.prod_pic = files.name;
             };
         },
 
@@ -106,91 +106,105 @@ export default {
 
         // 編輯確認按鈕點擊事件
         saveBtn() {
-            
-            const url = `${this.$store.state.phpPublicPath}editProd.php`;
-            let headers = {
-                Accept: "application/json",
-            };
-            console.log('1');
+            if (prod_id != undefined) {
+                const url = `${this.$store.state.phpPublicPath}editProd.php`;
+                // let headers = {
+                //     Accept: "application/json",
+                // };
+                const formData = new FormData();
+                formData.append("prod_id", this.editItem.prod_id);
+                formData.append("prod_name", this.editItem.prod_name);
+                formData.append("prod_price", this.editItem.prod_price);
+                formData.append("prod_singer", this.editItem.prod_singer);
+                formData.append("prod_date", this.editItem.prod_date);
+                formData.append("prod_type", this.editItem.prod_type);
+                formData.append("prod_inf", this.editItem.prod_inf);
+                formData.append("prod_int", this.editItem.prod_int);
+                formData.append("show_stat", this.editItem.show_stat);
+                //formData.append("prod_pic", document.getElementById("fileImg").files[0]);
 
-            const formData = new FormData();
-            formData.append("prod_id", this.editItem.prod_id);
-            formData.append("prod_name", this.editItem.prod_name);
-            formData.append("prod_price", this.editItem.prod_price);
-            formData.append("prod_singer", this.editItem.prod_singer);
-            formData.append("prod_date", this.editItem.prod_date);
-            formData.append("prod_type", this.editItem.prod_type);
-            formData.append("prod_inf", this.editItem.prod_inf);
-            formData.append("prod_int", this.editItem.prod_int);
-            formData.append("show_stat", this.editItem.show_stat);
-            formData.append("prod_pic", document.getElementById("fileImg").files[0]);
-            //console.log(document.getElementById("fileImg").files[0]);
+                if (document.getElementById("fileimg").files[0]) {
+                    formData.append("prod_pic", document.getElementById("fileimg").files[0]);
+                } else {
+                    formData.append("prod_pic", this.editItem.prod_pic);
+                }
 
-            //console.log(this.editItem);
-            //console.log("formData", formData);
-            fetch(url, {
-                method: "POST",
-                headers: headers,
-                body: formData,
-            })
-                .then((response) => {
-                    if (response.ok) {
-                        console.log(response);
-                        // return response.json();
-                    } else {
-                        throw new Error("編輯失敗");
-                    }
+                //console.log(document.getElementById("fileImg").files[0]);
+
+                //console.log(this.editItem);
+                //console.log("formData", formData);
+                fetch(url, {
+                    method: "POST",
+                    //headers: headers,
+                    body: formData,
                 })
-                .then(() => {
-                    //update this.productData
-                    let index = this.productData.findIndex((product) => {
-                        return product.prod_id == this.editItem.prod_id
+                    .then((response) => {
+                        if (response.ok) {
+                            console.log(response);
+                            // return response.json();
+                        } else {
+                            throw new Error("編輯失敗");
+                        }
+                    })
+                    .then(() => {
+                        //this.success(true, json);
+                        //this.fetchnew();
+                        this.editItem = [];
+                    })
+                    .catch((error) => {
+                        console.log(error.message);
                     });
-                    if (index >= 0) {
-                        //this.productData[index] = { ...this.editItem }
-                        this.productData.splice(index, 1, this.editItem);
-                        //console.log(index, "-------------", this.productData);
-                        //window.location.reload();
-                        this.fetchProdMgmt();
-                    } else {
-                        console.log("---error");
-                    }
-                    
-                })
-                .catch((error) => {
-                    console.error("發生錯誤:", error);
-                });
+                // then(() => {
+                //     //update this.productData
+                //     let index = this.productData.findIndex((product) => {
+                //         return product.prod_id == this.editItem.prod_id
+                //     });
+                //     if (index >= 0) {
+                //         //this.productData[index] = { ...this.editItem }
+                //         this.productData.splice(index, 1, this.editItem);
+                //         //console.log(index, "-------------", this.productData);
+                //         //window.location.reload();
+                //         this.fetchProdMgmt();
+                //     } else {
+                //         console.log("error.message");
+                //     }
+
+                // })
+                // .catch((error) => {
+                //     console.error("發生錯誤:", error);
+                // });
+            }
         },
         closeBtn() {
             this.editBox = false;
         }, // 關閉編輯跳窗
-        fetchProdMgmt(){
+        fetchProdMgmt() {
             //先檢查資料格式是否符合DB規則
-        const url = `${this.$store.state.phpPublicPath}postProdMgmt.php`;
-        let headers = {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-        };
-        fetch(url, {
-            method: "POST",
-            headers: headers,
-        })
-            .then((response) => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    console.error('API 錯誤回應:', response);
-                    throw new Error("取得 dta 失敗");
-                }
+            const url = `${this.$store.state.phpPublicPath}postProdMgmt.php`;
+            let headers = {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            };
+            fetch(url, {
+                method: "POST",
+                headers: headers,
             })
-            .then((json) => {
-                console.log('API 成功回應:', json);
-                this.productData = json;
-                // 將 prod_type 設定為正確的值
-            })
-            .catch((error) => {
-                console.error('API 請求失敗:', error);
-            });
+                .then((response) => {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        console.error('API 錯誤回應:', response);
+                        throw new Error("取得 dta 失敗");
+                    }
+                })
+                .then((json) => {
+                    console.log('API 成功回應:', json);
+                    this.productData = json;
+                    // 將 prod_type 設定為正確的值
+                })
+                .catch((error) => {
+                    console.error('API 請求失敗:', error);
+                });
         }
     },
     mounted() {
