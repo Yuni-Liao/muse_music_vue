e8b51d898a1ecc3fff3d70c30d236aad32b2f82c
rou94980 <rou94980@gmail.com>
2023-10-17 02:31:53 +0800

    feat 修正追蹤按鈕生命週期、為了測試有小微調在歌單排行榜的追蹤按鈕~

--- src/views/js/RankingView.js
+++ src/views/js/RankingView.js
@@ -10,10 +10,10 @@ import "swiper/css/pagination";
 import { Mousewheel, Pagination } from "swiper/modules";
 
 // -----------------------------------
-import player from '@/components/player.vue';
-import AddFavBtn from '@/components/AddFavBtn.vue';
-import AddSlBtn from '@/components/AddSlBtn.vue';
-import FolBtnBig from '@/components/FolBtnBig.vue';
+import player from "@/components/player.vue";
+import AddFavBtn from "@/components/AddFavBtn.vue";
+import AddSlBtn from "@/components/AddSlBtn.vue";
+import FolBtnBig from "@/components/FolBtnBig.vue";
 
 export default {
   components: {
@@ -33,7 +33,7 @@ export default {
     return {
       // 讓圖片 build 之後能顯示
       publicPath: process.env.BASE_URL,
-    
+
       SongRank: [],
       albumRank: [],
       SLRank: [],
@@ -42,39 +42,39 @@ export default {
   methods: {
     openPlayer() {
       this.$refs.player.playMusic();
-  },
-  gotosinglemusic(sid) {
-    this.$router.push({
-      name: "singlemusic",
-      params: {
-        sid,
-      },
-    });
-  },
-  gotosinglealbum(abid) {
-    let aa = this.$router.push({
-      name: "singlealbum",
-      params: {
-        salid: abid,
-      },
-    });
-  },
-  gotosonglist(slid) {
-    this.$router.push({
-      name: "singlesonglist",
-      params: {
-        slid,
-      },
-    });
-  },
-  gotosinger(memid) {
-    this.$router.push({
-      name: "profilepage",
-      params: {
-        memid,
-      },
-    });
-  },
+    },
+    gotosinglemusic(sid) {
+      this.$router.push({
+        name: "singlemusic",
+        params: {
+          sid,
+        },
+      });
+    },
+    gotosinglealbum(abid) {
+      let aa = this.$router.push({
+        name: "singlealbum",
+        params: {
+          salid: abid,
+        },
+      });
+    },
+    gotosonglist(slid) {
+      this.$router.push({
+        name: "singlesonglist",
+        params: {
+          slid,
+        },
+      });
+    },
+    gotosinger(memid) {
+      this.$router.push({
+        name: "profilepage",
+        params: {
+          memid,
+        },
+      });
+    },
   },
   mounted() {
     const fetchSongRank = () => {
@@ -85,7 +85,7 @@ export default {
       fetch(apiURL)
         .then((res) => res.json())
         .then((res) => {
-          this.SongRank = res; 
+          this.SongRank = res;
         })
         .catch((error) => {
           console.error("發生錯誤:", error);
@@ -103,11 +103,11 @@ export default {
         .then((res) => (this.albumRank = res))
         .catch((error) => {
           console.error("發生錯誤:", error);
-      });
+        });
     };
     fetchAlbumRank();
 
-    const fetchSLRank = () =>{
+    const fetchSLRank = () => {
       const apiURL = new URL(
         `http://localhost/muse_music/public/api/getRankSL.php`
       );
@@ -117,7 +117,7 @@ export default {
         .then((res) => (this.SLRank = res))
         .catch((error) => {
           console.error("發生錯誤:", error);
-      });
+        });
     };
     fetchSLRank();
   },
