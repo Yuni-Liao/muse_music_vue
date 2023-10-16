d7d9d8743929fb6ac8abf2cba42acb0135bba9c0
kay <candy168741@gmail.com>
2023-10-16 21:14:59 +0800

    update 後台 商品管理 跳窗改橫向

--- src/views/Backendviews/ProdMgmt.vue
+++ src/views/Backendviews/ProdMgmt.vue
@@ -17,7 +17,7 @@
     </div>
 
     <!-- 以下是表格資料區 -->
-    <Table highlight-row stripe class="prodmgmt_table cellHeight" width="1000" height="900" :columns="columns" :data="productData">
+    <Table highlight-row stripe class="prodmgmt_table cellHeight" width="1000" height="600" :columns="columns" :data="productData">
       <!-- 上下架按鈕 -->
       <template #upDownBtn="{ row }">
         <Switch v-model="row.show_stat" @on-change="toggleBtn(row)" false-color="#ff4949" true-color="#13ce66"
@@ -31,79 +31,92 @@
   </div>
   <!-- 以下是跳窗"編輯"區 -->
   <div v-if="editBox == true" class="editprod obj_Radius">
+    <div class="fir">
     <label for="prod_id" class="label">商品編號:</label>
-    <input type="text" id="prod_id" name="id" v-model="editItem.prod_id" style="margin-bottom: 10px; width: 550px"
+    <input type="text" id="prod_id" name="id" v-model="editItem.prod_id" style="margin-bottom: 10px; width: 300px"
       readonly />
     <label for="prod_name" class="label">商品名稱:</label>
     <input type="text" id="prod_name" name="name" v-model="editItem.prod_name"
-      style="margin-bottom: 10px; width: 550px" />
+      style="margin-bottom: 10px; width: 300px" />
     <label for="prod_singer" class="label">歌手名稱:</label>
     <input type="text" id="prod_singer" name="name" v-model="editItem.prod_singer"
-      style="margin-bottom: 10px; width: 550px" />
+      style="margin-bottom: 10px; width: 300px" />
     <label for="prod_price" class="label">商品售價:</label>
     <input type="text" id="prod_price" name="name" v-model="editItem.prod_price"
-      style="margin-bottom: 10px; width: 550px" />
+      style="margin-bottom: 10px; width: 300px" />
     <label for="prod_date" class="label">更新時間:</label>
-    <input type="text" id="prod_date" name="name" v-model="editItem.prod_date" style="margin-bottom: 10px; width: 550px"
+    <input type="text" id="prod_date" name="name" v-model="editItem.prod_date" style="margin-bottom: 10px; width: 300px"
       readonly />
     <label for="prod_type" class="label">商品分類:</label>
     <input type="text" id="prod_type" name="name" v-model="editItem.prod_type"
-      style="margin-bottom: 10px; width: 550px" />
-    <p>商品詳情:</p>
-    <textarea id="prod_inf" name="name" v-model="editItem.prod_inf"
-      style="margin-bottom: 10px; width: 550px; height: 80px"></textarea>
-    <p>商品介紹:</p>
-    <textarea id="prod_int" name="name" v-model="editItem.prod_int"
-      style="margin-bottom: 10px; width: 550px; height: 80px"></textarea>
-    <label for="show_stat" class="label">預設狀態:</label>
-    <select id="show_stat" style="margin-bottom: 10px; width: 550px" v-model="editItem.show_stat">
-      <option value="1">公開</option>
-      <option value="0">私人</option>
-    </select>
-    <p>圖片:</p>
-    <div class="uploadblock">
-      <label for="fileimg">
-        <p v-if="editItem.prod_pic == null">上傳圖片</p>
-        <p v-else>{{ editItem.prod_pic }}</p>
-
-        <input @change="img($event)" type="file" id="fileimg" name="img" style="display: none" />
-        <img v-if="editItem.prod_pic == null" :src="`${publicPath}image/ShopImage/shop_album0.jpg`" />
-        <img v-else-if="editItem.prod_pic != null && editItem.updateimg == false"
+      style="margin-bottom: 10px; width: 300px" />
+      </div>
+      <div class="sec">
+      <p>商品詳情:</p>
+      <textarea id="prod_inf" name="name" v-model="editItem.prod_inf"
+      style="margin-bottom: 10px; width: 300px; height: 80px"></textarea>
+      <p>商品介紹:</p>
+      <textarea id="prod_int" name="name" v-model="editItem.prod_int"
+      style="margin-bottom: 10px; width: 300px; height: 80px"></textarea>
+      <label for="show_stat" class="label state">預設狀態:</label>
+      <select id="show_stat" style="margin-bottom: 10px; width: 300px" v-model="editItem.show_stat">
+        <option value="1">公開</option>
+        <option value="0">私人</option>
+      </select>
+      <p>圖片:</p>
+      <div class="uploadblock">
+        <label for="fileimg">
+          <p v-if="editItem.prod_pic == null">上傳圖片</p>
+          <p v-else>{{ editItem.prod_pic }}</p>
+          
+          <input @change="img($event)" type="file" id="fileimg" name="img" style="display: none" />
+          <img v-if="editItem.prod_pic == null" :src="`${publicPath}image/ShopImage/shop_album0.jpg`" />
+          <img v-else-if="editItem.prod_pic != null && editItem.updateimg == false"
           :src="`${publicPath}image/ShopImage/${editItem.prod_pic}`" />
-        <img v-else-if="editItem.updateimg" :src="editItem.showimg" />
-      </label>
+          <img v-else-if="editItem.updateimg" :src="editItem.showimg" />
+        </label>
+      </div>
+      <div class="btngroup">
+        <button @click="closeBtn()" class="default_Btn obj_Radius closeBtn">
+          取消
+        </button>
+        <button @click.stop="saveEditBtn()" class="saveBtn default_Btn obj_Radius">
+          儲存
+        </button>
+        </div>
+      </div>
     </div>
-    <div class="btngroup">
-      <button @click="closeBtn()" class="default_Btn obj_Radius closeBtn">
-        取消
-      </button>
-      <button @click.stop="saveEditBtn()" class="saveBtn default_Btn obj_Radius">
-        儲存
-      </button>
-    </div>
-  </div>
+    
+
+
   <!-- 以下是跳窗"新增"區 -->
   <div v-if="addBox == true" class="addprod obj_Radius">
+    <div class="fir">
     <label for="addprod_name" class="label">商品名稱:</label>
     <input type="text" id="addprod_name" name="name" v-model="addItem.addprod_name"
-      style="margin-bottom: 10px; width: 550px" />
+      style="margin-bottom: 10px; width: 300px" />
     <label for="addprod_singer" class="label">歌手名稱:</label>
     <input type="text" id="addprod_singer" name="name" v-model="addItem.addprod_singer"
-      style="margin-bottom: 10px; width: 550px" />
+      style="margin-bottom: 10px; width: 300px" />
     <label for="addprod_price" class="label">商品售價:</label>
     <input type="text" id="addprod_price" name="name" v-model="addItem.addprod_price"
-      style="margin-bottom: 10px; width: 550px" />
+      style="margin-bottom: 10px; width: 300px" />
     <label for="addprod_type" class="label">商品分類:</label>
     <input type="text" id="addprod_type" name="name" v-model="addItem.addprod_type"
-      style="margin-bottom: 10px; width: 550px" />
+      style="margin-bottom: 10px; width: 300px" />
+    </div>
+
+    <div class="sec">
     <p>商品詳情:</p>
     <textarea id="addprod_inf" name="name" v-model="addItem.addprod_inf"
-      style="margin-bottom: 10px; width: 550px; height: 80px"></textarea>
+      style="margin-bottom: 10px; width: 300px; height: 80px"></textarea>
+
+      
     <p>商品介紹:</p>
     <textarea id="addprod_int" name="name" v-model="addItem.addprod_int"
-      style="margin-bottom: 10px; width: 550px; height: 80px"></textarea>
-    <label for="addshow_stat" class="label">預設狀態:</label>
-    <select id="addshow_stat" style="margin-bottom: 10px; width: 550px" v-model="addItem.addshow_stat">
+      style="margin-bottom: 10px; width: 300px; height: 80px"></textarea>
+    <label for="addshow_stat" class="label state">預設狀態:</label>
+    <select id="addshow_stat" style="margin-bottom: 10px; width: 300px" v-model="addItem.addshow_stat">
       <option value="1">公開</option>
       <option value="0">私人</option>
     </select>
@@ -127,6 +140,8 @@
         儲存
       </button>
     </div>
+  </div>
+  
   </div>
 </template>
 
