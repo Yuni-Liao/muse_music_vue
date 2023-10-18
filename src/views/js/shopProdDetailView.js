9a7585145bee35d9078018b1cfbb3e4eadd58719
kay <candy168741@gmail.com>
2023-10-18 15:15:02 +0800

    update 周邊販售 判斷選擇數量為0不加入購物車 非0則跳窗顯示"已加入購物車"

--- src/views/js/shopProdDetailView.js
+++ src/views/js/shopProdDetailView.js
@@ -31,33 +31,72 @@ export default {
             }
         },
         //這邊是 加入購物車
+        // addToCart() {
+        //     // 獲取已存儲的購物車數據或初始化一個空數組
+        //     const cartItemsJSON = localStorage.getItem('cartItems');
+        //     let cartItems = [];
+
+        //     if (cartItemsJSON) {
+        //         // 如果已經有購物車數據，則解析它
+        //         cartItems = JSON.parse(cartItemsJSON);
+        //     }
+
+        //     // 檢查購物車中是否已經存在相同的商品
+        //     const existingItem = cartItems.find(cartItem => cartItem.prod_name === this.foundObject.prod_name);
+
+        //     if (existingItem) {
+        //         // 如果存在相同的商品，將它們的 chat_num 數字相加
+        //         existingItem.chat_num += this.foundObject.chat_num;
+        //     } else {
+        //         // 如果不存在相同的商品，將商品添加到購物車數組
+        //         cartItems.push(this.foundObject);
+        //     }
+
+        //     // 將購物車數組轉換為 JSON 字符串
+        //     const updatedCartItemsJSON = JSON.stringify(cartItems);
+
+        //     // 使用 localStorage 存儲購物車數據
+        //     localStorage.setItem('cartItems', updatedCartItemsJSON);
+        // },
         addToCart() {
-            // 獲取已存儲的購物車數據或初始化一個空數組
-            const cartItemsJSON = localStorage.getItem('cartItems');
-            let cartItems = [];
 
-            if (cartItemsJSON) {
-                // 如果已經有購物車數據，則解析它
-                cartItems = JSON.parse(cartItemsJSON);
-            }
-
-            // 檢查購物車中是否已經存在相同的商品
-            const existingItem = cartItems.find(cartItem => cartItem.prod_name === this.foundObject.prod_name);
-
-            if (existingItem) {
-                // 如果存在相同的商品，將它們的 chat_num 數字相加
-                existingItem.chat_num += this.foundObject.chat_num;
+            if (this.foundObject.chat_num == 0) {
+                // 如果商品数量为0，显示警告信息
+                alert("請點選數量");
             } else {
-                // 如果不存在相同的商品，將商品添加到購物車數組
-                cartItems.push(this.foundObject);
+                // 獲取已存儲的購物車數據或初始化一個空數組
+                const cartItemsJSON = localStorage.getItem('cartItems');
+                let cartItems = [];
+
+                if (cartItemsJSON) {
+                    // 如果已經有購物車數據，則解析它
+                    cartItems = JSON.parse(cartItemsJSON);
+                }
+
+                // 檢查購物車中是否已經存在相同的商品
+                const existingItem = cartItems.find(cartItem => cartItem.prod_name === this.foundObject.prod_name);
+
+                if (existingItem) {
+                    // 如果存在相同的商品，將它們的 chat_num 數字相加
+                    existingItem.chat_num += this.foundObject.chat_num;
+                } else {
+                    // 如果不存在相同的商品，將商品添加到購物車數組
+                    cartItems.push(this.foundObject);
+                }
+
+                // 將購物車數組轉換為 JSON 字符串
+                const updatedCartItemsJSON = JSON.stringify(cartItems);
+
+                // 使用 localStorage 存儲購物車數據
+                localStorage.setItem('cartItems', updatedCartItemsJSON);
+
+                // 跳窗提醒
+                alert("已加入購物車");
+
+                // 將商品數量歸零
+                this.foundObject.chat_num = 0;
             }
-
-            // 將購物車數組轉換為 JSON 字符串
-            const updatedCartItemsJSON = JSON.stringify(cartItems);
-
-            // 使用 localStorage 存儲購物車數據
-            localStorage.setItem('cartItems', updatedCartItemsJSON);
-        },
+        }
     },
     //根據路由參數中的 id 值來查找並定位
     mounted() {
