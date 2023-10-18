dc9a59c30f5cae5f33397f8fba7f1e76df25ba73
rou94980 <rou94980@gmail.com>
2023-10-18 15:25:39 +0800

    恢復滾軸QQ

--- src/App.vue
+++ src/App.vue
@@ -10,24 +10,23 @@ html {
   /* height: 100%; */
 }
 
-@media screen and (max-width: 800px) {
-  body::-webkit-scrollbar {
-    /* width: 10px; */
-    background-color: #23232330;
-    border-radius: 10px;
-  }
-
-  body::-webkit-scrollbar-thumb {
-    border-radius: 10px;
-    background-image: -webkit-gradient(
-      linear,
-      left bottom,
-      left top,
-      color-stop(0.4, rgb(172, 182, 229)),
-      color-stop(0.8, rgb(116, 235, 213))
-    );
-  }
+body::-webkit-scrollbar {
+  width: 10px;
+  background-color: #23232330;
+  border-radius: 10px;
 }
+
+body::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  background-image: -webkit-gradient(
+    linear,
+    left bottom,
+    left top,
+    color-stop(0.4, rgb(172, 182, 229)),
+    color-stop(0.8, rgb(116, 235, 213))
+  );
+}
+
 body {
   -ms-overflow-style: scrollbar;
   /* height: 100%; */
