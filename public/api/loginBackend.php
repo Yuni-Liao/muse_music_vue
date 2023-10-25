246cf9261cb7889e1e5df51f5a6951e96eb8b1e8
Zachary <shinjinliao@gmail.com>
2023-10-25 20:39:44 +0800

    後台登入

--- public/api/loginBackend.php
+++ public/api/loginBackend.php
@@ -2,61 +2,34 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+require_once("./connectMusemusic.php");
+
+$response = [];
 
 try {
-    require_once("./connectMusemusic.php");
+    if (isset($_POST['acc']) && isset($_POST['admin_psw'])) {
+        $acc = trim($_POST['acc']);
+        $admin_psw = trim($_POST['admin_psw']);
+        
+        // 比對資料
+        $sql = "SELECT * FROM admin_acc WHERE acc=:acc AND admin_psw=:admin_psw";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(":acc", $acc);
+        $stmt->bindParam(":admin_psw", $admin_psw);
+        $stmt->execute();
 
-    if (isset($_POST['acc']) && isset($_POST['admin_psw'])) { 
-        $username = validate($_POST['acc']);
-        $password = validate($_POST['admin_psw']);
-
-        // 帳密輸入時，自動去掉空格~~~
-        $username = trim($username);
-        $password = trim($password);
-
-        if (empty($username) || empty($password)) {
-            echo json_encode(["error" => "帳號和密碼為必填欄位"]);
+        if ($stmt->rowCount() > 0) {
+            $response = ["success" => true, "message" => "登入成功"];
         } else {
-            // 綁定帳密做資料庫查詢
-            $sql = "SELECT * FROM admin_acc WHERE acc = :username AND admin_psw = :password";
-            $stmt = $pdo->prepare($sql);
-            $stmt->bindParam(":username", $username);
-            $stmt->bindParam(":password", $password);
-            $stmt->execute();
-
-            // 檢查帳密
-            if ($stmt->rowCount() > 0) {
-                // 查詢管理員帳密，新增id綁定
-                $sql = "SELECT admin_id, name FROM admin_acc WHERE acc = :username";
-                $stmt = $pdo->prepare($sql);
-                $stmt->bindParam(':username', $username);
-                $stmt->execute();
-                $result = $stmt->fetch(PDO::FETCH_ASSOC);
-
-                if ($result) {
-                    // 新增會員id
-                    $userId = $result['admin_id'];
-                    $userName = $result['name'];
-                    echo json_encode(["message" => "登入成功", "admin_id" => $userId, "name" => $userName]);
-                }
-            } else {
-                echo json_encode(["error" => "帳戶密碼不正確"]);
-            }
+            $response = ["message" => "帳號或密碼不正確"];
         }
     } else {
-        echo json_encode(["error" => "帳號和密碼為必填欄位"]);
+        $response = ["message" => "帳密沒有輸入"];
     }
 } catch (PDOException $e) {
-    // 錯誤處理 
-    $errorResponse = ["message" => "查詢失敗：" . $e->getMessage()];
-    echo json_encode($errorResponse);
+    $response = ["message" => "登入失敗：" . $e->getMessage()];
 }
 
-function validate($data)
-{
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
+header('Content-Type: application/json');
+echo json_encode($response);
 ?>
--- src/views/Backendviews/Login.vue
+++ src/views/Backendviews/Login.vue
@@ -5,9 +5,8 @@
     </div>
     <div class="backend_muse_login">
       <Login @on-submit="login">
-        <UserName name="username" v-model="admin_acc[0].acc" />
-        <Password name="password" v-model="admin_acc[0].admin_psw" />
-        <!-- <Submit /> -->
+        <UserName name="acc" v-model="admin_acc.acc" />
+        <Password name="admin_psw" v-model="admin_acc.admin_psw" />
         <Submit>
           <span slot="inner">登入</span>
         </Submit>
@@ -16,56 +15,27 @@
   </div>
 </template>
 
-<style scoped lang="scss">
-.backend_login_wrap {
-  height: 100vh;
-  display: flex;
-  flex-flow: column wrap;
-  justify-content: center;
-  align-items: center;
-  background-color: $dark;
-
-  .backend_muse_login {
-    width: 400px !important;
-    margin: 60px auto;
-  }
-
-  .backend_login_logo {
-    vertical-align: middle;
-  }
-}
-</style>
-
 <script>
 export default {
   data() {
     return {
       publicPath: process.env.BASE_URL,
-      admin_acc: [
-        {
-          acc: "請輸入帳號",
-          admin_psw: "",
-          name: "",
-        },
-      ],
-      login_admin_id: "",
-      emailInvalid: false,
-      passwordInvalid: false,
-      showPassword: false,
-      adminData: {},
+      admin_acc: {
+        acc: "",
+        admin_psw: "",
+      },
     };
   },
   methods: {
-    login() {
-      console.log("登入中...");
+    login(valid, { acc, admin_psw }) {
+      console.log(acc);
+      console.log(admin_psw);
 
       const dataToSend = new FormData();
+      dataToSend.append("acc", acc);
+      dataToSend.append("admin_psw", admin_psw);
 
-      dataToSend.append("acc", this.admin_acc[0].acc);
-      dataToSend.append("admin_psw", this.admin_acc[0].admin_psw);
-
-      const url = new URL(
-        `${this.$store.state.phpPublicPath}loginBackend.php`);
+      const url = new URL(`${this.$store.state.phpPublicPath}loginBackend.php`);
 
       fetch(url, {
         method: "POST",
@@ -75,19 +45,33 @@ export default {
         .then((data) => {
           if (data.message === "登入成功") {
             alert("登入成功");
-
-            const admin_id = data.admin_id;
-            const name = data.name;
-            localStorage.setItem("admin_id", admin_id);
-            localStorage.setItem("name", name);
-
             window.location.href = `${this.$store.state.linkPublicPath}backend`;
+            
           } else {
-            alert(data.error || "帳號密碼不正確");
+            alert(data.message || "帳號密碼不正確");
           }
         });
     },
-
   },
 };
 </script>
+
+<style scoped lang="scss">
+.backend_login_wrap {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: $dark;
+}
+
+  .backend_muse_login {
+    width: 400px !important;
+    margin: 60px auto;
+  }
+
+  .backend_login_logo {
+    vertical-align: middle;
+  }
+</style>
